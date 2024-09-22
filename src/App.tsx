240f6c0d068852097eee642d0662bfd6bec2ecc7
大沼 峻徳 <numa.san.1024@gmail.com>
2024-09-22 10:14:09 +0900

    アイコンを使えるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import { FC } from "react";
-import { Box } from "@mui/material";
+import { Box, Button, IconButton, Typography, Stack } from "@mui/material";
+import { ArrowLeft, ArrowRight, AccessAlarm } from "@mui/icons-material";
 
 const App: FC = () => {
-  return <Box sx={{ p: 1 }}>Hello World!</Box>;
+  return (
+    <Stack sx={{ p: 1 }} spacing={1}>
+      <Typography>Hello World!</Typography>
+      <Box>
+        <Button variant="contained" endIcon={<AccessAlarm />}>
+          アラームボタン
+        </Button>
+      </Box>
+      <Stack direction="row" alignItems="center">
+        <IconButton>
+          <ArrowLeft />
+        </IconButton>
+        <Typography>左右矢印ボタン</Typography>
+        <IconButton>
+          <ArrowRight />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default App;
